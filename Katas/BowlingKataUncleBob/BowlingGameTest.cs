e45f192b5be4d91ffb18be73266b4574cd199c33
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-17 13:39:13 +0200

    Test all ones

--- Katas/BowlingKataUncleBob/BowlingGameTest.cs
+++ Katas/BowlingKataUncleBob/BowlingGameTest.cs
@@ -5,30 +5,49 @@ namespace Katas.BowlingKataUncleBob;
 
 public sealed class BowlingGameTest
 {
+    private readonly Game g;
+
+    public BowlingGameTest()
+    {
+        g = new Game();
+    }
+
     [Fact]
     public void TestGutterGame()
     {
-        var g = new Game();
-        for (int i = 0; i < 20; i++)
-        {
-            g.Roll(0);
-        }
+        RollMany(20, 0);
 
         g.Score().Should().Be(0);
     }
-    
-    
+
+    [Fact]
+    public void TestAllOnes()
+    {
+        RollMany(20, 1);
+
+        g.Score().Should().Be(20);
+    }
+
+    private void RollMany(int n, int pins)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            g.Roll(pins);
+        }
+    }
 }
 
 public class Game
 {
+    private int score;
+    
     public void Roll(int pins)
     {
-        
+        score += pins;
     }
 
     public int Score()
     {
-        return 0;
+        return score;
     }
 }
