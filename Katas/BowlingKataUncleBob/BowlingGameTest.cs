d8584c9951c5a6d95ad933f67880d8f0daebeb21
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-17 13:27:44 +0200

    test gutter game

---
+++ Katas/BowlingKataUncleBob/BowlingGameTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Katas.BowlingKataUncleBob;
+
+public sealed class BowlingGameTest
+{
+    [Fact]
+    public void TestGutterGame()
+    {
+        var g = new Game();
+        for (int i = 0; i < 20; i++)
+        {
+            g.Roll(0);
+        }
+
+        g.Score().Should().Be(0);
+    }
+    
+    
+}
+
+public class Game
+{
+    public void Roll(int pins)
+    {
+        
+    }
+
+    public int Score()
+    {
+        return 0;
+    }
+}
