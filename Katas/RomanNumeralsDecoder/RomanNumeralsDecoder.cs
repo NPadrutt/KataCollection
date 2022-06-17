f246c91d0bc19b81cc4a57cf3c9492d1b958a74b
Nino Padrutt <nino.padrutt@hotmail.com>
2022-06-17 13:17:58 +0200

    use xUnit instead of NUnit

--- Katas/RomanNumeralsDecoder/RomanNumeralsDecoder.cs
+++ Katas/RomanNumeralsDecoder/RomanNumeralsDecoder.cs
@@ -1,34 +1,36 @@
 ﻿using System.Collections.Generic;
-using NUnit.Framework;
+using FluentAssertions;
+using Xunit;
 
 namespace Katas.RomanNumeralsDecoder;
 
-internal sealed class RomanNumeralsDecoder
+public sealed class RomanNumeralsDecoder
 {
     private class RomanDecode
     {
-        private static Dictionary<char, int> romanToArabic = new Dictionary<char, int>{
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
+        private static readonly Dictionary<char, int> romanToArabic = new()
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
         };
-      
+
         public static int Solution(string roman)
         {
-            int value = 0;
-            for(int i = 0; i < roman.Length; i++)
+            var value = 0;
+            for (var i = 0; i < roman.Length; i++)
             {
                 var currentNumber = romanToArabic[roman[i]];
-    
+
                 var nextIndex = i + 1;
                 if (nextIndex != roman.Length)
                 {
                     var nextNumber = romanToArabic[roman[nextIndex]];
-    
+
                     if (nextNumber > currentNumber)
                     {
                         value += nextNumber - currentNumber;
@@ -36,29 +38,30 @@ internal sealed class RomanNumeralsDecoder
                         continue;
                     }
                 }
-    
+
                 value += currentNumber;
             }
+
             return value;
         }
     }
-    
-    [TestFixture]
+
     public class RomanDecodeTests
     {
-        [TestCase(1, "I")]
-        [TestCase(2, "II")]
-        [TestCase(4, "IV")]
-        [TestCase(21, "XXI")]
-        [TestCase(500, "D")]
-        [TestCase(1000, "M")]
-        [TestCase(1954, "MCMLIV")]
-        [TestCase(1990, "MCMXC")]
-        [TestCase(2008, "MMVIII")]
-        [TestCase(2014, "MMXIV")]
+        [Theory]
+        [InlineData(1, "I")]
+        [InlineData(2, "II")]
+        [InlineData(4, "IV")]
+        [InlineData(21, "XXI")]
+        [InlineData(500, "D")]
+        [InlineData(1000, "M")]
+        [InlineData(1954, "MCMLIV")]
+        [InlineData(1990, "MCMXC")]
+        [InlineData(2008, "MMVIII")]
+        [InlineData(2014, "MMXIV")]
         public void Test(int expected, string roman)
         {
-            Assert.AreEqual(expected, RomanDecode.Solution(roman));
+            expected.Should().Be(RomanDecode.Solution(roman));
         }
     }
 }
